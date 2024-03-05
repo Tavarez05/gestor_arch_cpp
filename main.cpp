a1cbde7f88a61895bf9ce0186a97e26d91fb7796
Jose Tavarez <122190883+Tavarez05@users.noreply.github.com>
2024-03-05 19:29:15 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "hola mundo" << endl;
+    //esperar a que el usuario presione una tecla cout << "presione cualquier tecla para continuar..." << endl;
+    cin.get();
+
+    return 0;
+}
